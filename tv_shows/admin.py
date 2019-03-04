dc03931afa34805830d6c46094eb9f23bc511293
Mayara <mayaradiasg@gmail.com>
2019-03-04 02:35:05 -0300

    implemented tv_show app

--- tv_shows/admin.py
+++ tv_shows/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import TvShow
 # Register your models here.
+
+admin.site.register(TvShow)
--- tv_shows/models.py
+++ tv_shows/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from genres.models import Genre
 
 # Create your models here.
 class TvShow(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=30)
+    storyline = models.TextField()
+    average = models.DecimalField(decimal_places=1, max_digits=3)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
