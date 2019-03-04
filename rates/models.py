ed0e4bb3991291fa8306932f8560a302dd31cdfe
Mayara <mayaradiasg@gmail.com>
2019-03-04 02:12:10 -0300

    implemented rates app

--- rates/models.py
+++ rates/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tv_shows.models import TvShow
 
-# Create your models here.
+class Rate(models.Model):
+    rate = models.DecimalField(max_digits=3, decimal_places=1)
+    commentary = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tv_show = models.ForeignKey(TvShow, on_delete=models.CASCADE)
+
+
